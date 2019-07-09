9fbd2b67ac5ab354c3d9a7a5f72af44a77304c7a
hyunsu joo <hs9880@gmail.com>
2019-07-09 15:10:56 -0400

    remove unnecessary code

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,83 +1,41 @@
-$(window).on("load", function() {
-  $(".loader .inner").fadeOut(200, function() {
-    $(".loader").fadeOut(100);
-  });
-});
-
-$(document).ready(function() {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    items: 4,
-    nav: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      480: {
-        items: 2
-      },
-      768: {
-        items: 3
-      },
-      938: {
-        items: 4
-      }
-    }
-  });
-
-  var skillsTopOffset = $(".skills").offset().top;
-
-  $(window).scroll(function() {
-    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-      $(".chart").easyPieChart({
-        easing: "easeInOut",
-        barColor: "#eece1a",
-        trackColor: false,
-        scaleColor: false,
-        lineWidth: 4,
-        size: 140,
-        onStep: function(from, to, percent) {
-          $(this.el)
-            .find(".percent")
-            .text(Math.round(percent));
-        }
-      });
-    }
+$(window).on('load', function() {
+  $('.loader .inner').fadeOut(200, function() {
+    $('.loader').fadeOut(100);
   });
 });
 
 //show resume pdf
 
 function showDiv() {
-  const resume = document.getElementById("resume");
-  if (resume.style.display === "none") {
-    resume.style.display = "block";
+  const resume = document.getElementById('resume');
+  if (resume.style.display === 'none') {
+    resume.style.display = 'block';
   } else {
-    resume.style.display = "none";
+    resume.style.display = 'none';
   }
 }
 
 //select DOM Items
 
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-item");
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
 
 //set Initial State Of menu
 
 let showMenu = false;
 
-menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-  menuBtn.classList.toggle("close");
-  menu.classList.toggle("show");
-  menuBranding.classList.toggle("show");
-  menuNav.classList.toggle("show");
+  menuBtn.classList.toggle('close');
+  menu.classList.toggle('show');
+  menuBranding.classList.toggle('show');
+  menuNav.classList.toggle('show');
   navItems.forEach(item => {
-    item.classList.toggle("show");
+    item.classList.toggle('show');
   });
   showMenu = !showMenu;
 }
